9ceeacae0dcbac76ff8507122d5bed65028a6fd1
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-02-05 13:46:58 +0200

    Rebuild: 498 (2026-02-05 13:46:55)

--- home/desktop/waybar/config.nix
+++ home/desktop/waybar/config.nix
@@ -151,8 +151,8 @@
 
         wireplumber = {
           node-type = "Audio/Source";
-          format = "MIC";
-          format-muted = "<span color='#B96B6B'>MIC MUTE</span>";
+          format = "MIC ";
+          format-muted = "<span color='#B96B6B'>MIC MUTE</span> ";
           on-click = "pactl set-source-mute @DEFAULT_SOURCE@ toggle";
           tooltip-format = "{node_name}";
         };
