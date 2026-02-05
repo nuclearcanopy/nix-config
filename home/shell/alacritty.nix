19ddabe3d6da7df5c9aba1b23495761657231938
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-02-05 13:37:15 +0200

    Rebuild: 488 (2026-02-05 11:28:59)

--- home/shell/alacritty.nix
+++ home/shell/alacritty.nix
@@ -17,17 +17,17 @@
 
       colors = {
         primary = {
-          background = "#0f0f0f";
+          background = "#0c0c0c";
           foreground = "#d0d0d0";
         };
 
         cursor = {
-          text = "#0f0f0f";
+          text = "#0c0c0c";
           cursor = "#d0d0d0";
         };
 
         selection = {
-          text = "#0f0f0f";
+          text = "#0c0c0c";
           background = "#c0dfdd";
         };
         
