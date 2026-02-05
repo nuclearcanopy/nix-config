5503a62da435ad5893607e9d7a5b7bf88d999240
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-02-05 14:49:38 +0200

    Rebuild: 511 (2026-02-05 14:45:39)

--- home/desktop/mako.nix
+++ home/desktop/mako.nix
@@ -1,4 +1,10 @@
-{ config, pkgs, ... }:
+{ ... }:
+
 {
-  services.mako.enable = true;
+  services.mako = {
+    enable = true;
+    backgroundColor = "#1e1e1e";
+    borderColor = "#2e2e2e";
+    defaultTimeout = 7500;
+  }
 }
