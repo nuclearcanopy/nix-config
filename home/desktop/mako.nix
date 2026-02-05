45c13be0f8cff6c455f5a5bd584226aa8345f173
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-02-05 14:51:23 +0200

    Rebuild: 513 (2026-02-05 14:51:20)

--- home/desktop/mako.nix
+++ home/desktop/mako.nix
@@ -3,9 +3,9 @@
 {
   services.mako = {
     enable = true;
-    background-color = "#121212";
-    border-color = "#1f1f1f";
-    default-timeout = 7500;
+    backgroundColor = "#121212";
+    borderColor = "#1f1f1f";
+    defaultTimeout = 7500;
     padding = "10";
   };
 }
