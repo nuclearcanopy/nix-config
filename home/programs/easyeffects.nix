2d964224ac0811565779b438afa60b4fdaef77d1
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-02-06 10:31:51 +0200

    Rebuild: 551 (2026-02-06 10:31:48)

--- home/programs/easyeffects.nix
+++ home/programs/easyeffects.nix
@@ -3,23 +3,29 @@
 {
   home.packages = [ pkgs.easyeffects ];
 
-  xdg.configFile."easyeffects/db/bassEnhancerrc".text = ''
-    [soe][BassEnhancer#0]
-    amount=5.00
-    harmonics=8
-    scope=150
-  '';
+  xdg.configFile."easyeffects/db/bassEnhancerrc" = {
+    force = true;
+    text = ''
+      [soe][BassEnhancer#0]
+      amount=5.00
+      harmonics=8
+      scope=150
+    '';
+  };
 
-  xdg.configFile."easyeffects/easyeffectsrc".text = ''
-    [StreamInputs]
-    inputDevice=alsa_input.usb-audio-technica_AT2020USB_-00.analog-stereo
-    [StreamOutputs]
-    outputDevice=alsa_output.usb-FIIO_FiiO_K11_R2R-01.analog-stereo
-    plugins=bass_enhancer#0
-    visiblePage=pluginsPage
-    visiblePlugin=bass_enhancer#0
-    [Window]
-    height=1033
-    width=952
-  '';
+  xdg.configFile."easyeffects/easyeffectsrc" = {
+    force = true;
+    text = ''
+      [StreamInputs]
+      inputDevice=alsa_input.usb-audio-technica_AT2020USB_-00.analog-stereo
+      [StreamOutputs]
+      outputDevice=alsa_output.usb-FIIO_FiiO_K11_R2R-01.analog-stereo
+      plugins=bass_enhancer#0
+      visiblePage=pluginsPage
+      visiblePlugin=bass_enhancer#0
+      [Window]
+      height=1033
+      width=952
+    '';
+  };
 }
