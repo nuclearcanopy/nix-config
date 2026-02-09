6424cad986d7da30a246efc41fefde24163278b7
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-02-09 23:52:27 +0200

    Rebuild: 26 (2026-02-09 23:52:25)

--- home/shell/zsh/zsh.nix
+++ home/shell/zsh/zsh.nix
@@ -22,6 +22,7 @@
       nasmount = "sudo mount -t cifs //${secrets.nas.ip}/${secrets.nas.share} /mnt/nas -o username=${secrets.nas.username},iocharset=utf8,vers=3.1.1";
       sysd-ui = "systemd-manager-tui";
       vpnissue = "ip -s link show proton0 && sudo wg show";
+      fix-nvim = "sudo rm -f ~/.config/nvim/lazy-lock.json && sudo chown -R $USER:users ~/.local/share/nvim 2>/dev/null || true";
     };
 
     initContent = ''
