d28fbb600d4793a3b9c967e4590856fc283225f7
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-02-09 23:47:17 +0200

    Rebuild: 24 (2026-02-09 23:47:15)

--- system/default.nix
+++ system/default.nix
@@ -21,7 +21,7 @@
     # Network configuration
     ./network/base.nix
     ./network/storage.nix
-    #./network/tuning.nix
+    ./network/tuning.nix
 
     # System services
     ./services/system.nix
