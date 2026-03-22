da403079a864bb12c650655418c307946c4a3064
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-03-22 15:56:00 +0200

    Rebuild: 55 (2026-03-22 15:55:57)

--- system/core/packages.nix
+++ system/core/packages.nix
@@ -17,6 +17,7 @@
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
+      gamescopeSession.enable = true;
     };
     gamescope = {
       enable = true;
