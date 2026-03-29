696209c51abf313a2d6832cfc6ea5da14d1752be
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-03-29 16:13:06 +0300

    Rebuild: 88 (2026-03-29 16:13:04)

--- home/desktop/waybar/scripts/cpu_status.sh
+++ home/desktop/waybar/scripts/cpu_status.sh
@@ -2,4 +2,5 @@
 TEMP=$(cat /sys/class/hwmon/hwmon1/temp1_input 2>/dev/null)
 TEMP=$((TEMP / 1000))
 USAGE=$(awk '/^cpu / {usage=100-($5*100/($2+$3+$4+$5+$6+$7+$8))} END {printf "%.0f", usage}' /proc/stat)
-echo "CPU ${TEMP}° ${USAGE}%"
+((USAGE > 99)) && USAGE=99
+printf "CPU %02d° %02d%%\n" "$TEMP" "$USAGE"
--- home/desktop/waybar/scripts/gpu_status.sh
+++ home/desktop/waybar/scripts/gpu_status.sh
@@ -2,4 +2,5 @@
 TEMP=$(cat /sys/class/hwmon/hwmon5/temp2_input 2>/dev/null)
 TEMP=$((TEMP / 1000))
 USAGE=$(cat /sys/class/hwmon/hwmon5/device/gpu_busy_percent 2>/dev/null)
-echo "GPU ${TEMP}° ${USAGE}%"
+((USAGE > 99)) && USAGE=99
+printf "GPU %02d° %02d%%\n" "$TEMP" "$USAGE"
