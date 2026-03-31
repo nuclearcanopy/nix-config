2b39629019328dd75cbd9753b92ad900dc9d629d
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-03-31 21:08:25 +0300

    Rebuild: 104 (2026-03-31 21:08:22)

--- home/desktop/waybar/scripts/mouse_battery.sh
+++ home/desktop/waybar/scripts/mouse_battery.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RAW=$(cat /sys/bus/hid/drivers/razermouse/0003:1532:00B7.*/charge_level 2>/dev/null | head -1)
+RAW=$(cat /sys/bus/hid/drivers/razermouse/0003:1532:*.*/charge_level 2>/dev/null | head -1)
 
 if [[ -n "$RAW" ]]; then
     PERCENT=$((RAW * 100 / 255))
