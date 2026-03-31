781d4c11135ba12fbc28b0610982d35f7aed00a7
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-03-31 20:52:35 +0300

    Rebuild: 103 (2026-03-31 20:52:32)

--- system/hardware/openrazer.nix
+++ system/hardware/openrazer.nix
@@ -8,6 +8,10 @@
   nixpkgs.overlays = [
     (final: prev: {
       openrazer-daemon = unstable.openrazer-daemon;
+      # Also pull kernel module from unstable for newer kernel support
+      linuxPackages_zen = prev.linuxPackages_zen.extend (lpFinal: lpPrev: {
+        openrazer = unstable.linuxPackages_zen.openrazer;
+      });
     })
   ];
 
