e8958649a8b79acbccf3187114f2152084a14723
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-04-07 17:00:26 +0300

    Rebuild: 133 (2026-04-07 17:00:11)

--- modules/homeserver/users.nix
+++ modules/homeserver/users.nix
@@ -20,6 +20,11 @@
 
   users.defaultUserShell = pkgs.zsh;
 
+  # Codeberg host keys (declarative known_hosts)
+  programs.ssh.knownHosts = {
+    "codeberg.org".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVIC02vnjFyL+I4RHfvIGNtOgJMe769VTF1VR4EB3ZB";
+  };
+
   # SSH client config for Codeberg
   programs.ssh.extraConfig = ''
     Host codeberg.org
