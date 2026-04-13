782e386cca15d73dca24818a4be7588072426847
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-04-13 20:19:53 +0300

    removed openrazer causing kernel mismatch weirdness (Gen 152)

--- modules/system/hardware/openrazer.nix
+++ modules/system/hardware/openrazer.nix
@@ -8,10 +8,6 @@
   nixpkgs.overlays = [
     (final: prev: {
       openrazer-daemon = unstable.openrazer-daemon;
-      # newer kernel module
-      linuxPackages_zen = prev.linuxPackages_zen.extend (lpFinal: lpPrev: {
-        openrazer = unstable.linuxPackages_zen.openrazer;
-      });
     })
   ];
 
