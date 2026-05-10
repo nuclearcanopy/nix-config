38bbb2cf1a3c2690bf29ad76a0cb34ba74cd0906
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-05-10 17:08:07 +0300

    right alt instead of rigt control maybe? (Gen 6)

--- modules/shared/sway-base.nix
+++ modules/shared/sway-base.nix
@@ -81,21 +81,21 @@ in
         "${mod}+9" = "workspace number 9";
         "${mod}+0" = "workspace number 10";
 
-        "Control_R+1" = "move container to workspace number 1";
-        "Control_R+2" = "move container to workspace number 2";
-        "Control_R+3" = "move container to workspace number 3";
-        "Control_R+4" = "move container to workspace number 4";
-        "Control_R+5" = "move container to workspace number 5";
-        "Control_R+6" = "move container to workspace number 6";
-        "Control_R+7" = "move container to workspace number 7";
-        "Control_R+8" = "move container to workspace number 8";
-        "Control_R+9" = "move container to workspace number 9";
-        "Control_R+0" = "move container to workspace number 10";
+        "Alt_R+1" = "move container to workspace number 1";
+        "Alt_R+2" = "move container to workspace number 2";
+        "Alt_R+3" = "move container to workspace number 3";
+        "Alt_R+4" = "move container to workspace number 4";
+        "Alt_R+5" = "move container to workspace number 5";
+        "Alt_R+6" = "move container to workspace number 6";
+        "Alt_R+7" = "move container to workspace number 7";
+        "Alt_R+8" = "move container to workspace number 8";
+        "Alt_R+9" = "move container to workspace number 9";
+        "Alt_R+0" = "move container to workspace number 10";
 
-        "Control_R+a" = "move left";
-        "Control_R+d" = "move right";
-        "Control_R+w" = "move up";
-        "Control_R+s" = "move down";
+        "Alt_R+a" = "move left";
+        "Alt_R+d" = "move right";
+        "Alt_R+w" = "move up";
+        "Alt_R+s" = "move down";
 
         "${mod}+t" = "split toggle";
 
