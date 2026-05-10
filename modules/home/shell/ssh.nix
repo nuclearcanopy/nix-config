e4962c1405f0900754b9d7b3137a4a92c3252921
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-05-10 16:35:54 +0300

    move github known host to system level (host-base)

--- modules/home/shell/ssh.nix
+++ modules/home/shell/ssh.nix
@@ -5,13 +5,6 @@
     enable = true;
     enableDefaultConfig = false;
 
-    knownHosts = {
-      "github.com" = {
-        hostNames = [ "github.com" ];
-        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-      };
-    };
-
     matchBlocks = {
       "*" = {
         identitiesOnly = true;
