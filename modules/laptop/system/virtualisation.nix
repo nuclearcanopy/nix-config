aae7d8aa9fb95eea369fc64c8a31d2e8f022c064
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-05-24 14:44:28 +0300

    remove ovmf submodule (Gen 26)

--- modules/laptop/system/virtualisation.nix
+++ modules/laptop/system/virtualisation.nix
@@ -7,10 +7,6 @@
       package = pkgs.qemu_kvm;
       runAsRoot = false;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [ pkgs.OVMFFull ];
-      };
     };
   };
 
