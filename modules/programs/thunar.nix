685d99a2ab14734a325bed8e52e9ee5dd0fc7b8e
nuclearcanopy <git_l7a153rj3z@proton.me>
2026-07-12 01:43:20 +0300

    fix thunar issue (Gen 207)

--- modules/programs/thunar.nix
+++ modules/programs/thunar.nix
@@ -7,7 +7,7 @@
   nixos.modules.thunar = { pkgs, ... }: {
     programs.thunar = {
       enable = true;
-      plugins = with pkgs.xfce; [
+      plugins = with pkgs; [
         thunar-archive-plugin
         thunar-volman
       ];
